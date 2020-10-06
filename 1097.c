117fa866527cc5f593be09988e874d51510b8ca8
gggstavo <70299536+gggstavo@users.noreply.github.com>
2020-10-06 11:37:42 -0300

    Create 1097.c

---
+++ 1097.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+ int num = 1, cont = 7;
+ while (num <= 9){
+ printf("I=%d J=%d\n", num, cont --);
+ printf("I=%d J=%d\n", num, cont --);
+ printf("I=%d J=%d\n", num, cont --);
+num += 2;
+ cont += 5;
+ }
+ return 0;
+}
